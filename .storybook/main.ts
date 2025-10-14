4829a9cd3f2c332f88a1b979eae4124b29f826d6
Domusgpt <Phillips.paul.email@gmail.com>
2025-10-13 22:26:24 -0400

    Allow per-layer WGSL pipelines in WebGPU preview

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/html-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/stories/**/*.stories.@(ts|js)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/html-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  }
+};
+
+export default config;
